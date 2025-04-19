44a7f3b2494c0806724b963d984820a485e61ccb
Silvafilho070 <cursossilva070@gmail.com>
2025-04-18 22:25:00 -0300

    commit initial

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+function AddTask({ onTaskAdd }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md "
+        type="text"
+        placeholder="Digite o título da tarefa"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+
+      <input
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md "
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+
+      <button
+        onClick={() => {
+          //Verifica se o título e a descrição não estão vazios
+          if (!title.trim() || !description.trim()) {
+            return (
+              alert("Preencha todos os campos!") +
+              setTitle("") +
+              setDescription("")
+            );
+          }
+
+          onTaskAdd(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        //Adiciona a tarefa com o título e descrição
+        className={`bg-slate-500 text-white px-4 py-2 rounded-md font-medium`}
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,59 @@
+import{ ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+function Tasks(props) {
+  const navigate = useNavigate();
+  //Função que navega para a página da tarefa
+
+  function onSeeDetailsClick(task){
+    const query = new URLSearchParams()
+    query.set("title", task.title)
+    query.set("description", task.description)
+
+    navigate(`/task?${query.toString()}`)
+
+  }
+
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {/* Itera sobre as tarefas e cria um item de lista para cada uma */}
+      {/* A chave é o id da tarefa */}
+      {/* O botão chama a função onTaskClick passando o id da tarefa */}
+      {/* O botão tem a classe "line-through" se a tarefa estiver concluída */}
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white p-2 rounded-md w-full text-left ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
+
+          <button
+            onClick={() => 
+              onSeeDetailsClick(task)
+            }
+            className="bg-slate-400 text-white p-2 rounded-md">
+            <ChevronRightIcon />                                  
+          </button>
+
+          <button 
+            //Passa o id da tarefa para a função onTaskDelete
+            //A função onTaskDelete é passada como props
+            //A função onTaskDelete remove a tarefa da lista
+            //A função onTaskDelete é chamada quando o botão é clicado
+            onClick={() => props.onTaskDelete(task.id)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
+            <TrashIcon />                                  
+          </button>
+
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
